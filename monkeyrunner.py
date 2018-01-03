b2b7421d43c9e61e65b734538c8337b4d3dfc0cc
Kainan Zhu <dlangu0393@gmail.com>
2018-01-03 13:54:37 +0800

    Add jitter support in monkeyrunner.py

--- monkeyrunner.py
+++ monkeyrunner.py
@@ -6,21 +6,18 @@ import signal
 
 from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice, MonkeyView
 
-if len(sys.argv) != 3:
-    print 'usage: monkeyrunner %s deviceId remoteUri' % sys.argv[0]
-    sys.exit(1)
-
-deviceId = sys.argv[1]
-remote = sys.argv[2]
-
-print 'Connecting', deviceId
-
-device = MonkeyRunner.waitForConnection(timeout=60, deviceId=deviceId)
-
-print 'Connected'
+device_id = None
+remote_url = None
 
 
-def main():
+def connect():
+    print 'Connecting', device_id
+    device = MonkeyRunner.waitForConnection(timeout=60, deviceId=device_id)
+    print 'Connected'
+    return device
+
+
+def main(device):
     raw_input('Press enter to start')
 
     while True:
@@ -28,8 +25,8 @@ def main():
         snapshot = device.takeSnapshot()
         img_data = snapshot.convertToBytes('png')
 
-        print 'Requesting...', remote
-        req = urllib2.Request(remote, img_data)
+        print 'Requesting...', remote_url
+        req = urllib2.Request(remote_url, img_data)
         req.add_header('Content-Length', str(len(img_data)))
         req.add_header('Content-Type', 'application/octet-stream')
         try:
@@ -55,13 +52,75 @@ def exit_handler(signum, frame):
     print 'Exiting...'
     try:
         subprocess.call(
-            "adb -s %s shell kill -9 $(adb -s %s shell ps | grep monkey | awk '{print $2}')" % (deviceId, deviceId),
+            "adb -s %s shell kill -9 $(adb -s %s shell ps | grep monkey | awk '{print $2}')"
+            % (device_id, device_id),
             shell=True)
     except Exception, e:
         print e
     sys.exit(0)
 
 
+def parse_args():
+    """Jython does not provide argparse package, parse arguments by hand."""
+
+    def usage(error=None):
+        if error is not None:
+            print error
+            print ''
+
+        print 'usage: monkeyrunner monkeyrunner.py [-h] [--jitter JITTER_FLOAT]'
+        print '                                    DEVICE_ID REMOTE_URL'
+        print ''
+        print 'arguments:'
+        print '  DEVICE_ID     target device id'
+        print '  REMOTE_URL    remote server url'
+        print ''
+        print 'optional arguments:'
+        print '  -h, --help    show this help message and exit'
+        print '  --jitter JITTER_FLOAT'
+        print '                press time will be multiplied by a random value'
+        print '                in range [1 - JITTER_FLOAT, 1 + JITTER_FLOAT]'
+
+        sys.exit(1 if error is not None else 0)
+
+    argv = sys.argv[1:]
+
+    if '-h' in argv or '--help' in argv:
+        usage()
+
+    jitter = None
+    if '--jitter' in argv:
+        jitter_idx = argv.index('--jitter')
+        if jitter_idx == len(argv) - 1:
+            usage('--jitter expects a float number')
+        try:
+            jitter = float(argv[jitter_idx + 1])
+        except ValueError:
+            usage('--jitter expects a float number, %r got\n' %
+                  (argv[jitter_idx + 1], ))
+        del argv[jitter_idx:jitter_idx + 2]
+
+    if len(argv) != 2:
+        usage('missing arguments')
+
+    device_id, remote_url = argv
+
+    return {'jitter': jitter, 'device_id': device_id, 'remote_url': remote_url}
+
+
 if __name__ == '__main__':
+    args = parse_args()
+    device_id = args['device_id']
+    remote_url = args['remote_url']
+
+    print 'Device id:', device_id
+    print 'Remote url:', remote_url
+
+    if args['jitter']:
+        remote_url += '?' if '?' not in remote_url else '&'
+        remote_url += 'jitter=%s' % (args['jitter'], )
+
     signal.signal(signal.SIGINT, exit_handler)
-    main()
+
+    device = connect()
+    main(device)
